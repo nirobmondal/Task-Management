0e67331feb25e69d0718a17cfe13fdff2be8edf7
nirobmondal <nirobmondal202@gmail.com>
2025-11-03 17:36:15 +0600

    /src/routes/tasks.js file created, all task and task by id route moved to that file and test the route with postman to check successful movement

--- src/index.js
+++ src/index.js
@@ -1,44 +1,7 @@
 const express = require("express");
 const app = express();
 const port = 3000;
-
-const tasks = [
-  {
-    id: 1,
-    title: "Learn Node.js",
-    completed: false,
-    priority: "high",
-    createdAt: new Date(),
-  },
-  {
-    id: 2,
-    title: "Build REST API",
-    completed: false,
-    priority: "medium",
-    createdAt: new Date(),
-  },
-  {
-    id: 3,
-    title: "Test API with Postman",
-    completed: true,
-    priority: "medium",
-    createdAt: new Date(),
-  },
-  {
-    id: 4,
-    title: "Refactor routes using Express Router",
-    completed: false,
-    priority: "low",
-    createdAt: new Date(),
-  },
-  {
-    id: 5,
-    title: "Document API responses",
-    completed: false,
-    priority: "high",
-    createdAt: new Date(),
-  },
-];
+const taskRouter = require("./routes/tasks");
 
 const health = [
   {
@@ -51,18 +14,8 @@ app.get("/", (req, res) => {
   res.send("Task Management API is running!");
 });
 
-app.get("/tasks", (req, res) => {
-  res.json(tasks);
-});
-
-app.get("/task/:id", (req, res) => {
-  const id = Number(req.params.id);
-
-  const task = tasks.find((t) => t.id === id);
-  if (!task) return res.status(404).json({ error: "Task not found" });
-
-  res.json(task);
-});
+app.use("/tasks", taskRouter);
+app.use("/task", taskRouter);
 
 app.get("/health", (req, res) => {
   res.json(health);
---
+++ src/routes/tasks.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const router = express.Router();
+
+const tasks = [
+  {
+    id: 1,
+    title: "Learn Node.js",
+    completed: false,
+    priority: "high",
+    createdAt: new Date(),
+  },
+  {
+    id: 2,
+    title: "Build REST API",
+    completed: false,
+    priority: "medium",
+    createdAt: new Date(),
+  },
+  {
+    id: 3,
+    title: "Test API with Postman",
+    completed: true,
+    priority: "medium",
+    createdAt: new Date(),
+  },
+  {
+    id: 4,
+    title: "Refactor routes using Express Router",
+    completed: false,
+    priority: "low",
+    createdAt: new Date(),
+  },
+  {
+    id: 5,
+    title: "Document API responses",
+    completed: false,
+    priority: "high",
+    createdAt: new Date(),
+  },
+];
+
+
+router.get("/", (req, res) => {
+  res.json(tasks);
+});
+
+router.get("/:id", (req, res) => {
+  const id = Number(req.params.id);
+
+  const task = tasks.find((t) => t.id === id);
+  if (!task) return res.status(404).json({ error: "Task not found" });
+
+  res.json(task);
+});
+
+module.exports = router;
